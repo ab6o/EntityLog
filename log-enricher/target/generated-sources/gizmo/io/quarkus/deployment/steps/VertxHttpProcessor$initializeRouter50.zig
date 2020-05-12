efbeb7175ec790b623d4a2893cca5696076fb96a
Anand <anandhi@flipkart.com>
2020-05-12 10:39:27 +0530

    Remove target folder

---
+++ log-enricher/target/generated-sources/gizmo/io/quarkus/deployment/steps/VertxHttpProcessor$initializeRouter50.zig
@@ -0,0 +1,93 @@
+// Class: io/quarkus/deployment/steps/VertxHttpProcessor$initializeRouter50
+//     Access =  public synthetic
+//     Extends: java/lang/Object
+//     Implements:
+//         io/quarkus/runtime/StartupTask
+
+// DO NOT MODIFY.  This is not actually a source file; it is a textual representation of generated code.
+// Use only for debugging purposes.
+
+// Auto-generated constructor
+// Access: public
+Method <init> : V
+(
+    // (no arguments)
+) {
+    ALOAD 0
+    // Method descriptor: ()V
+    INVOKESPECIAL java/lang/Object#<init>
+    RETURN
+    
+}
+
+// Access: public
+Method deploy_0 : V
+(
+    arg 1 = Lio/quarkus/runtime/StartupContext;,
+    arg 2 = [Ljava/lang/Object;
+) {
+    ** label1
+    NEW io/quarkus/vertx/http/runtime/VertxHttpRecorder
+    DUP
+    // Method descriptor: ()V
+    INVOKESPECIAL io/quarkus/vertx/http/runtime/VertxHttpRecorder#<init>
+    ASTORE 3
+    ALOAD 2
+    LDC (Integer) 0
+    ALOAD 3
+    AASTORE
+    ALOAD 1
+    LDC (String) "proxykey112"
+    // Method descriptor: (Ljava/lang/String;)Ljava/lang/Object;
+    INVOKEVIRTUAL io/quarkus/runtime/StartupContext#getValue
+    ASTORE 6
+    LDC (String) "NORMAL"
+    // Method descriptor: (Ljava/lang/String;)Lio/quarkus/runtime/LaunchMode;
+    INVOKESTATIC io/quarkus/runtime/LaunchMode#valueOf
+    ASTORE 4
+    ALOAD 1
+    LDC (String) "io.quarkus.runtime.ShutdownContext"
+    // Method descriptor: (Ljava/lang/String;)Ljava/lang/Object;
+    INVOKEVIRTUAL io/quarkus/runtime/StartupContext#getValue
+    ASTORE 5
+    ALOAD 2
+    LDC (Integer) 0
+    AALOAD
+    CHECKCAST io/quarkus/vertx/http/runtime/VertxHttpRecorder
+    ALOAD 6
+    CHECKCAST io/quarkus/runtime/RuntimeValue
+    ALOAD 4
+    ALOAD 5
+    CHECKCAST io/quarkus/runtime/ShutdownContext
+    // Method descriptor: (Lio/quarkus/runtime/RuntimeValue;Lio/quarkus/runtime/LaunchMode;Lio/quarkus/runtime/ShutdownContext;)Lio/quarkus/runtime/RuntimeValue;
+    INVOKEVIRTUAL io/quarkus/vertx/http/runtime/VertxHttpRecorder#initializeRouter
+    ASTORE 7
+    ALOAD 1
+    LDC (String) "proxykey122"
+    ALOAD 7
+    // Method descriptor: (Ljava/lang/String;Ljava/lang/Object;)V
+    INVOKEVIRTUAL io/quarkus/runtime/StartupContext#putValue
+    RETURN
+    ** label2
+    
+}
+
+// Access: public
+Method deploy : V
+(
+    arg 1 = Lio/quarkus/runtime/StartupContext;
+) {
+    ** label1
+    LDC (Integer) 1
+    ANEWARRAY java/lang/Object
+    ASTORE 2
+    ALOAD 0
+    ALOAD 1
+    ALOAD 2
+    // Method descriptor: (Lio/quarkus/runtime/StartupContext;[Ljava/lang/Object;)V
+    INVOKEVIRTUAL io/quarkus/deployment/steps/VertxHttpProcessor$initializeRouter50#deploy_0
+    RETURN
+    ** label2
+    
+}
+
