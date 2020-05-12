0c35851765d34c6f6374c729bcf0215f7582690c
Anand <anandhi@flipkart.com>
2020-05-12 11:23:59 +0530

    Remove target folder

--- vegetables-service/target/classes/import.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE purchase_orders REPLICA IDENTITY FULL;
-ALTER TABLE purchase_order_lines REPLICA IDENTITY FULL;
-INSERT INTO vegetable (id, name, description) VALUES (1, 'Potato', 'Spicy');
-
-CREATE TABLE purchase_orders (id INTEGER, order_type VARCHAR, amount DECIMAL, date VARCHAR, from_business_id VARCHAR, to_business_id VARCHAR)
-CREATE TABLE purchase_order_lines (id INTEGER, order_id INTEGER, product_id VARCHAR, amount DECIMAL, quantity INTEGER)
